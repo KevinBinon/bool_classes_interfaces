2e666e676f98b89eb358708ea42a3bfb04e69217
Mattia Romeo <mattiaromeo@gmail.com>
2018-09-24 15:58:18 +0200

    mattiar: basic setup

---
+++ src/main/java/accessability_mutability/SimplePojoPizza.java
@@ -0,0 +1,8 @@
+package accessability_mutability;
+
+public class SimplePojoPizza {
+//    Create a pizza class it should have a name, price, list with toppings, ...
+//    Make this class restrict accessability as much as possible
+//    Make this class immutable
+
+}
