7199a77d003563370cf77f5ad27cdbbfdb20cb56
Mattia Romeo <mattiaromeo@gmail.com>
2018-09-24 16:03:56 +0200

    mattiar: basic setup continued

--- src/main/java/accessability_mutability/SimplePojoPizza.java
+++ src/main/java/accessability_mutability/SimplePojoPizza.java
@@ -5,4 +5,38 @@ public class SimplePojoPizza {
 //    Make this class restrict accessability as much as possible
 //    Make this class immutable
 
+    private String name;
+    private double price;
+    private String[] toppings;
+
+    public SimplePojoPizza(String name, double price, String[] toppings) {
+
+        this.name = name;
+        this.price = price;
+        this.toppings = toppings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String[] getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String[] toppings) {
+        this.toppings = toppings;
+    }
 }
